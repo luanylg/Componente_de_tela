38d6aeee5a25aa49a7d8ba228683ab0d4166421e
♡Luzinha♡ <125080324+luanylg@users.noreply.github.com>
2024-03-26 08:44:02 -0300

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,39 @@
+import { Text, View, TextInput, TouchableOpacity, FlatList } from "react-native"
+import Card from "../../component/Card"
+import { estilos } from "./styles"
+
+export default function Home(){
+
+    const lista = []//'teste', 'luany', 'bruno', 'chico', 'yasmmin', 'ket', 'lulis'
+
+    return(
+        <View style = {estilos.container}>
+       
+        <Text style= {estilos.titulo}>Pesquisa...</Text>
+        <View style = {estilos.form}>
+        <TouchableOpacity style = {estilos.botom}>
+        <Text style = {estilos.text_img}>
+                👸🏼
+                </Text>
+            </TouchableOpacity>
+
+                <TextInput style = {estilos.input_text}/>
+
+               <TouchableOpacity style = {estilos.botao}>
+                <Text style = {estilos.texto_botao}>
+                🔎
+                </Text>
+               </TouchableOpacity>
+            </View>
+
+            <FlatList 
+                data={lista}
+                keyExtractor={item => item}
+                renderItem={({item})=>(
+                    <Card key={item} name={item} />
+                )}
+                showsVerticalScrollIndicador={false}
+            />
+        </View>
+        )
+    }
